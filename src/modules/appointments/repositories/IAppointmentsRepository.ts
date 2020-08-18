b589f61adbad7d506fd1301a1a2dcd6897f1fd6c
Thales Morais <thales.morais21@gmail.com>
2020-08-17 21:59:00 -0300

    added service ListProviderDayAvailability

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,10 +1,12 @@
 import Appointment from "../infra/typeorm/entities/Appointment";
 import ICreateAppointmentDTO from './../dtos/ICreateAppointmentDTO';
 import IFindAllInMontFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayProviderDTO from '../dtos/IFindAllInDayFromProviderDTO';
 
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
   findAllInMonthFromProvider(data: IFindAllInMontFromProviderDTO): Promise<Appointment[]>;
+  findAllInDayFromProvider(data: IFindAllInDayProviderDTO): Promise<Appointment[]>
 }
--- src/modules/appointments/services/ListProviderMonthAvaliability.ts
+++ src/modules/appointments/services/ListProviderMonthAvaliability.ts
@@ -3,6 +3,7 @@
 
 import 'reflect-metadata'
 import {inject, injectable} from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns'
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -35,7 +36,7 @@ class ListProviderMonthAvailabilityService {
 
     public async execute({provider_id, month, year }: IRequest): Promise<IResponse> {
 
-      const appointments = this.appointmentsRepository.findAllInMonthFromProvider(
+      const appointments = await this.appointmentsRepository.findAllInMonthFromProvider (
         {
           provider_id: provider_id,
           year,
@@ -44,13 +45,29 @@ class ListProviderMonthAvailabilityService {
       );
 
 
-      console.log(appointments);
-        return [
-            {
-              day: 1,
-              available: false
+        const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+
+
+        const eachDayArray = Array.from(
+          { length: numberOfDaysInMonth},
+          (value, index) => index + 1,
+        );
+
+
+
+        const availability = eachDayArray.map(day => {
+            const appointmentsInDay = appointments.filter(appointment => {
+              return getDate(appointment.date) === day;
+            });
+
+            return {
+              day: day,
+              available: appointmentsInDay.length < 10,
             }
-        ]
+        });
+
+
+        return availability;
     }
 
 
